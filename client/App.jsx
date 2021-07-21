616eb4165d465e93f641358efb96b9805c1a910a
James Raden <james.raden@gmail.com>
2021-07-20 23:30:26 -0400

    Add data-driven functionality to the App component

--- client/App.jsx
+++ client/App.jsx
@@ -2,6 +2,25 @@
 
 import React from "react";
 
-export const App = () => {
-  return <div>This is a react component.</div>;
+export const App = ({ questions, answers }) => {
+  return (
+    <div>
+      <h1>Q&A Tool</h1>
+
+      {questions.map(({ questionId, content }, i) => (
+        <div key={questionId}>
+          <h3>{content}</h3>
+          <div>
+            {answers
+              .filter((answer) => answer.questionId == questionId)
+              .map(({ content, upvotes, answerId }) => (
+                <div key={answerId}>
+                  {content} - {upvotes}
+                </div>
+              ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
--- server/index.js
+++ server/index.js
@@ -7,6 +7,58 @@ import { renderToString } from "react-dom/server";
 
 import { App } from "../client/App";
 
+const data = {
+  questions: [
+    {
+      questionId: "Q1",
+      content: "Which back end solution should we use for our application?",
+    },
+    {
+      questionId: "Q2",
+      content:
+        "What percentage of developer time should be devoted to end-to-end testing?",
+    },
+  ],
+  answers: [
+    {
+      answerId: "A1",
+      questionId: 1,
+      upvotes: 2,
+      content: "Apache",
+    },
+    {
+      answerId: "A2",
+      questionId: "Q1",
+      upvotes: 0,
+      content: "Java",
+    },
+    {
+      answerId: "A3",
+      questionId: "Q1",
+      upvotes: 4,
+      content: "Node.js",
+    },
+    {
+      answerId: "A4",
+      questionId: "Q2",
+      upvotes: 2,
+      content: "25%",
+    },
+    {
+      answerId: "A5",
+      questionId: "Q2",
+      upvotes: 1,
+      content: "50%",
+    },
+    {
+      answerId: "A6",
+      questionId: "Q2",
+      upvotes: 1,
+      content: "75%",
+    },
+  ],
+};
+
 const app = express();
 
 app.use(express.static("dist"));
@@ -14,7 +66,7 @@ app.use(express.static("dist"));
 app.get("/", (req, res) => {
   // res.send("<h1>React is excellent!</h1>");
   const index = readFileSync("public/index.html", "utf-8");
-  const rendered = renderToString(<App />);
+  const rendered = renderToString(<App {...data} />);
   res.send(index.replace("{{rendered}}", rendered));
 });
 
